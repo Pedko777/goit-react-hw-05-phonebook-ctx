b2152eeb12b5ff02b361190a871c99082f61dc1a
Pedko777 <yuriipedko@gmail.com>
2020-07-08 19:55:26 +0300

    Updates

---
+++ precache-manifest.44852ccf9592ba4f89e3a880cf6d2b76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec0c318e12f5a46b34081326b782b86d",
+    "url": "/goit-react-hw-05-phonebook-ctx/index.html"
+  },
+  {
+    "revision": "0e32ff4459d968dc2035",
+    "url": "/goit-react-hw-05-phonebook-ctx/static/css/main.d6223146.chunk.css"
+  },
+  {
+    "revision": "c045d883975a13c27711",
+    "url": "/goit-react-hw-05-phonebook-ctx/static/js/2.e35b9ebd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-05-phonebook-ctx/static/js/2.e35b9ebd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e32ff4459d968dc2035",
+    "url": "/goit-react-hw-05-phonebook-ctx/static/js/main.071e7150.chunk.js"
+  },
+  {
+    "revision": "54dd34004e0b6cdfb6f4",
+    "url": "/goit-react-hw-05-phonebook-ctx/static/js/runtime-main.1121389a.js"
+  }
+]);
